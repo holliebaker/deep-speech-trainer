ec99c20682623c572516e365b58fc57c34a5f32a
Hollie Baker <hollieabaker@gmail.com>
2020-04-22 21:17:11 +0100

    Messy UI

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>The quick brown starfox jumped over the lazy duck.</Text>
+
+      <View>
+        <Button
+          title='Record'
+          onPress={_ => _}
+        />
+        <Button
+          title='Play Back Recording'
+          onPress={_ => _}
+        />
+        <Button
+          title='Upload Recording'
+          onPress={_ => _}
+        />
+      </View>
     </View>
   )
 }
