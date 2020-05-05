188d39f4cf5291fb614d9e35b588dac6b699e467
Hollie Baker <hollieabaker@gmail.com>
2020-05-05 20:22:36 +0100

    Make get request for snippets

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react'
 
 import { fetchSentence } from './api'
+import ErrorScreen from './ErrorScreen'
 import LoadingScreen from './LoadingScreen'
 import RecordingScreen from './RecordingScreen'
 
 const App = () => {
   const [sentence, setSentence] = useState(null)
+  const [error, setError] = useState(null)
   useEffect(() => {
-    fetchSentence().then(newSentence =>
-      setSentence(newSentence)
-    )
+    fetchSentence().then(({ snippet }) =>
+      setSentence(snippet)
+    ).catch(e => {
+      console.log(e)
+
+      setError(e)
+    })
   }, [])
+
+  if (error) {
+    return(
+      <ErrorScreen error={error} />
+    )
+  }
 
   if (!sentence) {
     return <LoadingScreen />
--- api.js
+++ api.js
@@ -1,4 +1,7 @@
-// TODO API calls
+import { get } from 'react-native-axios' 
+
+const BASE_URL = 'https://api.hameed.info'
 
 export const fetchSentence = () =>
-  Promise.resolve('dummy sentence...')
+  get(BASE_URL + '/speech/v1.0/snippets/')
+    .then(({ data }) => data)
