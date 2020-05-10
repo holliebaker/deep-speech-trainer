9e2be7e6a80f4fbeddf716d48a6f701e61d0ebe7
Hollie Baker <hollieabaker@gmail.com>
2020-05-10 13:49:14 +0100

    Do not mock api requests

--- api.js
+++ api.js
@@ -3,9 +3,9 @@ import { get, patch } from 'react-native-axios'
 const BASE_URL = 'https://api.hameed.info'
 
 export const fetchSentence = () =>
-//  get(BASE_URL + '/speech/v1.0/snippets/')
-//    .then(({ data }) => data)
-  Promise.resolve({ snippet: 'hello world' })
+  get(BASE_URL + '/speech/v1.0/snippets/')
+    .then(({ data }) => data)
+
 export const submitRecording = (audio, { id, token }) =>
   patch(BASE_URL + '/speech/v1.0/snippets/' + id, {
     id,
