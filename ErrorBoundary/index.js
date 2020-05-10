063f41148a8185c4bfc679b783a2fbd3e6c94f27
Hollie Baker <hollieabaker@gmail.com>
2020-05-10 10:48:40 +0100

    Wrap app in error boundary

---
+++ ErrorBoundary/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import ErrorScreen from '../ErrorScreen'
+
+class ErrorBoundary extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { error: null }
+  }
+
+  static getDerivedStateFromError (error) {
+    return { error }
+  }
+
+  componentDidCatch (error, errorInfo) {
+    console.log(error, errorInfo)
+  }
+
+  render () {
+    if (this.state.error) {
+      console.log("should catch")
+      return <ErrorScreen error={this.state.error} />
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
