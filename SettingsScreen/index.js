339a5772eaef32166fe982605ce1f529e6acf34b
Hollie Baker <hollieabaker@gmail.com>
2020-05-13 21:03:57 +0100

    create settings ui

--- SettingsScreen/index.js
+++ SettingsScreen/index.js
@@ -1,9 +1,32 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput } from 'react-native'
+
+import styles from '../util/styles'
 
 const SettingsScreen = () => {
+  const [url, setUrl] = useState('')
+  const [user, setUser] = useState('')
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View>
+        <Text>Server URL:</Text>
+
+        <TextInput
+          onChongeText={setUrl}
+          value={url}
+        />
+      </View>
+
+      <View>
+        <Text>Api User:</Text>
+
+        <TextInput
+          onChongeText={setUser}
+          value={user}
+        />
+      </View>
+    </View>
   )
 }
 
